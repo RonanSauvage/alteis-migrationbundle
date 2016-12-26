ac265b990a8d1a780f8f256e805991b6aa75189c
Ronan Sauvage <rsauvage@alteis.fr>
2016-12-26 10:21:35 +0100

    Correctif définition migration

--- Alteis/MigrationBundle/DependencyInjection/Configuration.php
+++ Alteis/MigrationBundle/DependencyInjection/Configuration.php
@@ -19,6 +19,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
+        $treeBuilder->root('alteis_migration');
         return $treeBuilder;
     }
 }
